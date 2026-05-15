afc07a75fea44e8f08314d4ffcda1cd4fb009e64
vinod chappidi <chappidivinod74@gmail.com>
2026-05-15 16:23:58 +0530

    second:SpeedInsights

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "@/index.css";
 import App from "@/App";
 
@@ -7,5 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 );
